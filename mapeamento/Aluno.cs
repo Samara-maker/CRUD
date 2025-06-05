ddf213e25d637b81d57d515d5baa8a68ae531f1d
Samara Hespanhol <samarahespanhol@gmail.com>
2025-06-04 21:54:58 -0400

    1

---
+++ mapeamento/Aluno.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUD.mapeamento
+{
+    internal class Aluno
+    {
+
+        public string id_aluno;
+        public string nome;
+        public string CPF;
+        public DateTime DataNasc;
+        public string sexo;
+        public string altura;
+        public string telefone;
+
+
+
+
+    }
+}
---
+++ utilitario/Conexao.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+//baixar pacote Nuget, para conectar com o banco de dados = MySql.data
+
+
+namespace CRUD.utilitario
+{
+    internal class Conexao
+    {
+
+       public static MySqlConnection conexao;
+
+        public static MySqlConnection Conectar()
+        {
+            try
+            {
+                string strconexao = "serve:localhost;uid=root;pwd=root;database=FitnessTraining";
+                conexao = new MySqlConnection(strconexao);
+                conexao.Open();
+                return conexao;
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("ERRO AO CONECTAR COM O BANCO DE DADOS" + ex.Message);
+            }
+
+        }
+        public static void FecharConexao()
+        {
+            conexao.Close();
+        }
+
+
+
+    }
+}
